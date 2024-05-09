3f063da68b773a654cdaa286bd480df6f028e5dd
Nik Pietanze <nikpietanze@gmail.com>
2024-05-09 18:32:51 -0500

    at least it runs?

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,33 +2,43 @@ import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./Components/Sidebar";
 import Home from "./Home";
-import Server from "./Server";
 
 interface Server {
-	name: string;
+    name: string;
 }
 
 interface User {
-	avatar: string;
-	email: string;
-	servers: Server[] | null;
+    avatar: string;
+    email: string;
+    servers: Server[] | null;
 }
-export const UserContext = React.createContext<User>({} as User);
+
+//export const UserContext = React.createContext({} as User);
+
+/*
+        <BrowserRouter>
+            <UserContext.Provider value={{ user, setUser }}>
+                <main className="flex min-h-screen">
+                    <Sidebar />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </main>
+            </UserContext.Provider>
+        </BrowserRouter>
+*/
 
 export default function App() {
-	const [user, setUser] = useState<User>({} as User);
+    //const [user, setUser] = useState({});
 
-	return (
-		<BrowserRouter>
-			<UserContext.Provider value={{ user: user, setUser: setUser }}>
-				<main className="flex min-h-screen">
-					<Sidebar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/:serverName" element={<Server />} />
-					</Routes>
-				</main>
-			</UserContext.Provider>
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <main className="flex min-h-screen">
+                <Sidebar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
 }
